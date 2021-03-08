88a1aa49ed78cd5b9ddfbdf9f0e3f5db50881e54
HaDoCk-wq <71933423+HaDoCk-wq@users.noreply.github.com>
2021-03-08 16:57:25 +0100

    Add files via upload

--- js/ajax.js
+++ js/ajax.js
@@ -9,8 +9,35 @@ class ajaxClass {
             ajaxBody.data = null;
         }
         let req = new XMLHttpRequest();
+        let done;
         req.open(ajaxBody.method, ajaxBody.url, true);
+        if (this.done) {
+            done = this.done;
+        }
+        if (this.loading) {
+            this.loading(req);
+        }
         req.onreadystatechange = function () {
+            if (ajaxBody.unsent) {
+                if (req.readyState == 0) {
+                    ajaxBody.unsent(req);
+                }
+            }
+            if (ajaxBody.opened) {
+                if (req.readyState == 1) {
+                    ajaxBody.opened(req);
+                }
+            }
+            if (ajaxBody.headers_received) {
+                if (req.readyState == 2) {
+                    ajaxBody.headers_received(req);
+                }
+            }
+            if (ajaxBody.loading) {
+                if (req.readyState == 3) {
+                    ajaxBody.loading(req);
+                }
+            }
             if (req.readyState == 4) {
                 if (req.status == 200) {
                     if (ajaxBody.success) {
@@ -20,6 +47,132 @@ class ajaxClass {
                         else {
                             ajaxBody.success(req.responseText);
                         }
+                    }
+                    if (done) {
+                        done(req);
+                    }
+                }
+                else {
+                    if (ajaxBody.error) {
+                        ajaxBody.error(req);
+                    }
+                }
+                if (ajaxBody.done) {
+                    ajaxBody.done(req);
+                }
+            }
+        };
+        req.send(ajaxBody.data);
+    }
+    async post(ajaxBody) {
+        if (ajaxBody.data == undefined) {
+            ajaxBody.data = null;
+        }
+        let req = new XMLHttpRequest();
+        let done;
+        req.open("post", ajaxBody.url, true);
+        if (this.done) {
+            done = this.done;
+        }
+        if (this.loading) {
+            this.loading(req);
+        }
+        req.onreadystatechange = function () {
+            if (ajaxBody.unsent) {
+                if (req.readyState == 0) {
+                    ajaxBody.unsent(req);
+                }
+            }
+            if (ajaxBody.opened) {
+                if (req.readyState == 1) {
+                    ajaxBody.opened(req);
+                }
+            }
+            if (ajaxBody.headers_received) {
+                if (req.readyState == 2) {
+                    ajaxBody.headers_received(req);
+                }
+            }
+            if (ajaxBody.loading) {
+                if (req.readyState == 3) {
+                    ajaxBody.loading(req);
+                }
+            }
+            if (req.readyState == 4) {
+                if (req.status == 200) {
+                    if (ajaxBody.success) {
+                        if (ajaxBody.object) {
+                            ajaxBody.success(JSON.parse(req.responseText));
+                        }
+                        else {
+                            ajaxBody.success(req.responseText);
+                        }
+                    }
+                    if (done) {
+                        done(req);
+                    }
+                }
+                else {
+                    if (ajaxBody.error) {
+                        ajaxBody.error(req);
+                    }
+                }
+                if (ajaxBody.done) {
+                    ajaxBody.done(req);
+                }
+            }
+        };
+        req.send(ajaxBody.data);
+    }
+    async get(ajaxBody) {
+        if (ajaxBody.method == undefined) {
+            ajaxBody.method = "GET";
+        }
+        if (ajaxBody.data == undefined) {
+            ajaxBody.data = null;
+        }
+        let req = new XMLHttpRequest();
+        let done;
+        req.open("get", ajaxBody.url, true);
+        if (this.done) {
+            done = this.done;
+        }
+        if (this.loading) {
+            this.loading(req);
+        }
+        req.onreadystatechange = function () {
+            if (ajaxBody.unsent) {
+                if (req.readyState == 0) {
+                    ajaxBody.unsent(req);
+                }
+            }
+            if (ajaxBody.opened) {
+                if (req.readyState == 1) {
+                    ajaxBody.opened(req);
+                }
+            }
+            if (ajaxBody.headers_received) {
+                if (req.readyState == 2) {
+                    ajaxBody.headers_received(req);
+                }
+            }
+            if (ajaxBody.loading) {
+                if (req.readyState == 3) {
+                    ajaxBody.loading(req);
+                }
+            }
+            if (req.readyState == 4) {
+                if (req.status == 200) {
+                    if (ajaxBody.success) {
+                        if (ajaxBody.object) {
+                            ajaxBody.success(JSON.parse(req.responseText));
+                        }
+                        else {
+                            ajaxBody.success(req.responseText);
+                        }
+                    }
+                    if (done) {
+                        done(req);
                     }
                 }
                 else {
@@ -35,37 +188,6 @@ class ajaxClass {
         req.send(ajaxBody.data);
     }
     async load(ajaxBody, id) {
-        if (ajaxBody.method == undefined) {
-            ajaxBody.method = "GET";
-        }
-        if (ajaxBody.data == undefined) {
-            ajaxBody.data = null;
-        }
-        let req = new XMLHttpRequest();
-        req.open(ajaxBody.method, ajaxBody.url, true);
-        req.onreadystatechange = function () {
-            if (req.readyState == 4) {
-                if (req.status == 200) {
-                    if (ajaxBody.success) {
-                        if (ajaxBody.object) {
-                            ajaxBody.success(JSON.parse(req.responseText));
-                        }
-                        else {
-                            ajaxBody.success(req.responseText);
-                        }
-                    }
-                }
-                else {
-                    if (ajaxBody.error) {
-                        ajaxBody.error(req);
-                    }
-                }
-                if (ajaxBody.done) {
-                    ajaxBody.done(req);
-                }
-            }
-        };
-        req.send(ajaxBody.data);
     }
 }
 const http = new ajaxClass();
